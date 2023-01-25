c5f84583a649b1d64ac9c0c3aa612f585945e44b
staillebois <sylvain.taillebois@gmail.com>
2023-01-25 16:41:28 +0100

    Allow to send specific list of animes

--- mangamix/settings.py
+++ mangamix/settings.py
@@ -8,5 +8,6 @@ S3_HOST = os.getenv('S3_HOST', 'localhost:9000')
 S3_ACCESS_KEY = os.getenv('S3_ACCESS_KEY', 'minio')
 S3_SECRET_KEY = os.getenv('S3_SECRET_KEY', 'minio123')
 S3_BUCKET = os.getenv('S3_BUCKET', 'mangamix')
-MMX_EXTRACT_LIMIT = int(os.getenv('MMX_EXTRACT_LIMIT', '15'))
+MMX_EXTRACT_LIMIT = int(os.getenv('MMX_EXTRACT_LIMIT', '1'))
 MMX_SEARCH_SIZE = int(os.getenv('MMX_SEARCH_SIZE', '10'))
+MMX_ANIMES = os.getenv('MMX_ANIMES').split(',') if os.getenv('MMX_ANIMES') else []
